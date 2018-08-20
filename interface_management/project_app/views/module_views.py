2fa9faf6b6cb6720006edc7e08f5ea89c9883c05
fnngj <fnngj@126.com>
2018-08-20 16:22:38 +0800

    添加模块接口. email:fnngj@126.com

--- interface_management/project_app/views/module_views.py
+++ interface_management/project_app/views/module_views.py
@@ -1,5 +1,6 @@
 from interface_management import common
 from project_app.daos.project_dao import ProjectDao
+from project_app.daos.module_dao import ModuleDao
 
 
 def add_module(request):
@@ -8,7 +9,27 @@ def add_module(request):
     :param request:
     :return:
     """
-    pass
+    if request.method == "POST":
+        req = common.json_to_dict(request.body)
+        project = common.get_request_key(req, "porject")
+        name = common.get_request_key(req, "name")
+        describe = common.get_request_key(req, "describe")
+
+        if project is None or name is None or describe is None:
+            return common.response_failed("必传参数为空")
+
+        req = ProjectDao.get_object_by_name(name)
+        if req is None:
+            return common.response_failed("项目查询错误")
+        
+        if len(req) != 0:
+            req = ModuleDao.create(req[0], name, describe)
+            if req is None:
+                return common.response_failed("创建模块失败")
+
+        return common.response_succeed("创建成功")
+    else:
+        return common.response_failed("请求方法错误")
 
 
 def update_module(request):
