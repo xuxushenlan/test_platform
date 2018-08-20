6215415aeeb7cecf3be368d6458011fd447b74b5
fnngj <fnngj@126.com>
2018-08-20 19:05:21 +0800

    测试git hooks. emial:fnngj@126.com

--- interface_management/project_app/urls.py
+++ interface_management/project_app/urls.py
@@ -16,5 +16,4 @@ urlpatterns = [
     path('update_module', module_views.update_module),
     path('get_modules', module_views.get_modules),
     path('del_module', module_views.delete_module),
-
 ]
