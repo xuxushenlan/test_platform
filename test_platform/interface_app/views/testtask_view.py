59261a457350e7f5b6a58ba05409030359fefa79
fnngj <fnngj@126.com>
2018-12-05 00:03:30 +0800

    任务运行实现部分

--- test_platform/interface_app/views/testtask_view.py
+++ test_platform/interface_app/views/testtask_view.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from interface_app.models import TestCase
 from interface_app.models import TestTask
 
 """
@@ -28,3 +29,23 @@ def add_task(request):
     else:
         return HttpResponse("404")
 
+# 运行测试任务
+def run_task(request, tid):
+    if request.method == "GET":
+        task_obj = TestTask.objects.get(id=tid)
+        cases_str = task_obj.cases
+        cases_list = cases_str.split(",")
+        print("666", cases_list)
+        for i in cases_list:
+            try:
+                int(i)
+            except ValueError:
+                continue
+            else:
+                case_obj = TestCase.objects.get(id=i)
+                print(case_obj.name)
+        
+        return HttpResponseRedirect('/interface/task_manage')
+
+    else:
+        return HttpResponse("404")
